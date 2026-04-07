2575da8738a164d2b9283fd42d241e434a6b5a2f
José Daniel <jose.jdfonseca@gmail.com>
2026-04-07 20:01:31 -0300

    Aprendendo a utilizar Statment e ResultSet para ler dados do banco

--- jdbc-conectivty/src/Main.java
+++ jdbc-conectivty/src/Main.java
@@ -1,12 +1,42 @@
+import java.io.IOException;
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        Connection conn = DB.open();
-        DB.close();
+
+        //Cria a conexão com o banco de dados.
+        Connection conn = null;
+
+        //Serve para montar um comando SQL a ser executado.
+        Statement st = null;
+
+        //Representará um objeto contendo o resultado da consulta em forma de tabela.
+        ResultSet rs = null;
+
+        try {
+            //Abrindo a conexão com o banco.
+            conn = DB.open();
+
+            //Criando o Statment a partir da conexão com o banco de dados
+            st = conn.createStatement();
+
+            //Montando o comando SQL e atribuindo o valor que retornará ao ResultSet.
+            rs = st.executeQuery("select * from seller");
+
+            //Percorrendo o ResultSet com o comando rs.next() e definindo quais campos da tabela quero pegar.
+            while (rs.next()) {
+                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            }
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
 
 
 
