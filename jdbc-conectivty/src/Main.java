467cf68d105a65884122c97047dffa4616cefd2e
José Daniel <jose.jdfonseca@gmail.com>
2026-04-13 22:26:21 -0300

    Aprendendo a adicionar dados no MySQL

--- jdbc-conectivty/src/Main.java
+++ jdbc-conectivty/src/Main.java
@@ -1,46 +1,53 @@
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+
+import java.sql.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-
-        //Cria a conexão com o banco de dados.
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Connection conn = null;
-
-        //Serve para montar um comando SQL a ser executado.
-        Statement st = null;
-
-        //Representará um objeto contendo o resultado da consulta em forma de tabela.
-        ResultSet rs = null;
-
+        PreparedStatement st = null;
         try {
-            //Abrindo a conexão com o banco.
             conn = DB.open();
 
-            //Criando o Statment a partir da conexão com o banco de dados
-            st = conn.createStatement();
+            st = conn.prepareStatement(
+                    "INSERT INTO seller "
+                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
+                            + "VALUES "
+                            + "(?, ?, ?, ?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
 
-            //Montando o comando SQL e atribuindo o valor que retornará ao ResultSet.
-            rs = st.executeQuery("select * from seller");
+                st.setString(1, "Zezin");
+            st.setString(2, "zezin@gmail.com");
+            st.setDate(3, new java.sql.Date(sdf.parse("18/04/2026").getTime()));
+            st.setDouble(4, 3000.0);
+            st.setInt(5, 4);
 
-            //Percorrendo o ResultSet com o comando rs.next() e definindo quais campos da tabela quero pegar.
-            while (rs.next()) {
-                System.out.println(rs.getInt("Id") + ", " + rs.getString("Name"));
+            int rowsAffected = st.executeUpdate();
+
+            if (rowsAffected > 0) {
+                ResultSet rs = st.getGeneratedKeys();
+                while (rs.next()) {
+                    int id = rs.getInt(1);
+                    System.out.println("Done! Id = " + id);
+                }
+            }
+            else {
+                System.out.println("No rown Affected");
             }
         }
         catch (SQLException e) {
             e.printStackTrace();
         }
+        catch (ParseException e) {
+            e.printStackTrace();
+        }
         finally {
-            //Fechando o statment, o resultSet e o banco
             DB.closeStatment(st);
-            DB.closeResultSet(rs);
             DB.close();
         }
     }
