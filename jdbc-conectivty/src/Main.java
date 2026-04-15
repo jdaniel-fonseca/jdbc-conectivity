aea9c01e02d444aa4417f85cad296cb6a1a75735
José Daniel <jose.jdfonseca@gmail.com>
2026-04-14 21:05:52 -0300

    Atualizando dados no MySQL

--- jdbc-conectivty/src/Main.java
+++ jdbc-conectivty/src/Main.java
@@ -1,54 +1,35 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
         Connection conn = null;
         PreparedStatement st = null;
+
         try {
             conn = DB.open();
 
             st = conn.prepareStatement(
-                    "INSERT INTO seller "
-                            + "(Name, Email, BirthDate, BaseSalary, DepartmentId)"
-                            + "VALUES "
-                            + "(?, ?, ?, ?, ?)",
-                    Statement.RETURN_GENERATED_KEYS);
+                    "UPDATE seller "
+                    + "SET BaseSalary = BaseSalary + ? "
+                    + "WHERE "
+                    + "(DepartmentID = ?)");
 
-                st.setString(1, "Zezin");
-            st.setString(2, "zezin@gmail.com");
-            st.setDate(3, new java.sql.Date(sdf.parse("18/04/2026").getTime()));
-            st.setDouble(4, 3000.0);
-            st.setInt(5, 4);
+            st.setDouble(1, 200);
+            st.setInt(2, 2);
 
             int rowsAffected = st.executeUpdate();
-
-            if (rowsAffected > 0) {
-                ResultSet rs = st.getGeneratedKeys();
-                while (rs.next()) {
-                    int id = rs.getInt(1);
-                    System.out.println("Done! Id = " + id);
-                }
-            }
-            else {
-                System.out.println("No rown Affected");
-            }
+            System.out.println("Done! rows affected: " + rowsAffected);
         }
         catch (SQLException e) {
             e.printStackTrace();
         }
-        catch (ParseException e) {
-            e.printStackTrace();
-        }
         finally {
-            DB.closeStatment(st);
             DB.close();
+            DB.closeStatment(st);
         }
+
     }
 }
